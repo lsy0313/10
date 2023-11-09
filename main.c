7024c2f74a342b080846e996f04fbba4edb9b43b
lsy0313 <03lsy13@naver.com>
2023-11-09 15:18:26 +0900

    실습1

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ROWS 3
+#define COLS 3
+
+int main(int argc, char *argv[])
+{
+  char code1 = 'A';
+  int code2 = 65;
+  
+  printf("code1 = %c, (%d)\n", code1, code1);
+  printf("code2 = %c, (%d)\n", code2, code2);
+
+  system("PAUSE");	
+  return 0;
+}
+
+
